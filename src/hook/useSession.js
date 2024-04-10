0ddeeb1ca72962d8d29c4fd20e81d63f135efe84
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-04-09 23:07:32 -0500

    1o commit

--- src/hook/useSession.js
+++ src/hook/useSession.js
@@ -1,23 +1,23 @@
 const useSession = () => {
     const login = (userData) => {
-        if (process.browser) {
+        if (typeof sessionStorage !== 'undefined') {
             sessionStorage.setItem('logged', true);
             sessionStorage.setItem('userData', JSON.stringify(userData));
         }
     };
 
     const logout = () => {
-        if (process.browser) {
+        if (typeof sessionStorage !== 'undefined') {
             sessionStorage.clear();
         }
     };
 
     const getUserData = () => {
-        if (process.browser) {
+        if (typeof sessionStorage !== 'undefined') {
             const userData = sessionStorage.getItem('userData');
             return userData ? JSON.parse(userData) : null;
         }
-        return null;
+        return null; // O manejar de otra manera si sessionStorage no está disponible
     };
 
     return { login, logout, getUserData };
